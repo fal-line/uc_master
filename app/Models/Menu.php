cf8adeae2989869fcbd234ba7c22a15ba2e1994e
fal-line <mnaufal.ihza@gmail.com>
2025-06-07 11:56:16 +0700

    adding Menu model fillable

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'category',
+        'price',
+        'most_ordered',
+        'img_url',
+    ];
 }
