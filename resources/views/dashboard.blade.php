9b4d42f1b49de59f7f6b8277486a62680b9597a1
fal-line <mnaufal.ihza@gmail.com>
2025-06-07 17:51:18 +0700

    update dashboard link to image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
     <div class="card bg-dark text-white rounded-3">
         <a href="/dashboard/menu" class="text-white">
 
-            <img src="img/dashboard_placeholder.png" class="card-img">
+            <img src="img/menu_placeholder.png" class="card-img">
             <div class="card-img-overlay">
                 <h2 class="card-title mt-3">Menu Management</h2>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -26,7 +26,7 @@
     <div class="card bg-dark text-white rounded-3">
         <a href="/dashboard/crew" class="text-white">
 
-            <img src="img/dashboard_placeholder.png" class="card-img">
+            <img src="img/crew_placeholder.png" class="card-img">
             <div class="card-img-overlay">
                 <h2 class="card-title mt-3">Crew Management</h2>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -41,7 +41,7 @@
     <div class="card bg-dark text-white rounded-3">
         <a href="/dashboard/order" class="text-white">
 
-            <img src="img/dashboard_placeholder.png" class="card-img">
+            <img src="img/order_placeholder.png" class="card-img">
             <div class="card-img-overlay">
                 <h2 class="card-title mt-3">Order Management</h2>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -56,7 +56,7 @@
     <div class="card bg-dark text-white rounded-3">
         <a href="/dashboard/payment" class="text-white">
 
-            <img src="img/dashboard_placeholder.png" class="card-img">
+            <img src="img/payment_placeholder.png" class="card-img">
             <div class="card-img-overlay">
                 <h2 class="card-title mt-3">Peyment Management</h2>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
