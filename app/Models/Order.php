a526caf6bd08d9e4b1f3ab4a5c69468c2e806172
fal-line <mnaufal.ihza@gmail.com>
2025-06-10 01:56:43 +0700

    add Order model for excel

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * Menentukan nama tabel yang terhubung dengan model ini.
+     *
+     * @var string
+     */
+    protected $table = 'order';
+
+
+    protected $fillable = [
+        'id',
+        'total',
+        'amountPaid',
+        'amountChange',
+        'customer',
+        'status',
+        'user_id',
+        'payReference',
+    ];
+}
